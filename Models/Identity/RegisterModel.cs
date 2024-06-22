edce000522a397f5dbb548994b5cbc063cf8ffd2
Nguyen Viet Hoang <nguyenviethoang.2003.personal@gmail.com>
2024-06-22 20:28:33 +0700

    feat: chat

---
+++ Models/Identity/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaClient.Models.Identity
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ Share/RequestModels/RegisterReq.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaClient.Share.RequestModels
+{
+    public sealed class SignInReq
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; init; }
+
+        [Required]
+        public string UserName { get; init; }
+
+        [Required]
+        public string Password { get; init; }
+
+        
+        [Required]
+        public string ConfirmPassword { get; init; }
+    }
+}
